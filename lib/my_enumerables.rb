1efb47681ccc40a83f5b36e83e3cb16d1a716009
Michael Johnson <z17.mcj@gmail.com>
2026-05-13 10:43:21 -0600

    still working on my_reduce

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -96,29 +96,52 @@ module Enumerable
     ret_value
   end
 
-  def my_inject(n = nil)
-    #Doing some research on how this works.
-    #if I provide an initial value that is where the accumulator will begin
-    #if no initial value is provided the first element of the collection is used as the starting value
-    #It will take two arguments, the accumulator and element, accumulator going first.
-    #the accumulator is commonly called the "memo"
-    #I'm not sure about hashes and the different outputs I'm expected to be able to
-    #produce. I know in the normal inject I can have the output be a hash. I'm not sure
-    #which data type to make my accumulator.
-    #My research also didn't tell me if it passes the enumerator if no block is passed, but I will
-    #start off assuming that is how it works.
-    return to_eunm(:my_inject) unless block_given?
+  # def my_inject(n = nil)
+  #   #Doing some research on how this works.
+  #   #if I provide an initial value that is where the accumulator will begin
+  #   #if no initial value is provided the first element of the collection is used as the starting value
+  #   #It will take two arguments, the accumulator and element, accumulator going first.
+  #   #the accumulator is commonly called the "memo"
+  #   #I'm not sure about hashes and the different outputs I'm expected to be able to
+  #   #produce. I know in the normal inject I can have the output be a hash. I'm not sure
+  #   #which data type to make my accumulator.
+  #   #My research also didn't tell me if it passes the enumerator if no block is passed, but I will
+  #   #start off assuming that is how it works.
+  #   return to_eunm(:my_inject) unless block_given?
 
-    ret_value = 0
-    if n == nil
-      ret_value = self[0]
-    else
-      ret_value = n
-    end
+  #   memo = 0
+  #   memo = self[0] if n == nil
+        
+  #   self.my_each do |v|
+  #     memo += yield(memo, v)
+  #   end
+  #   memo
+  # end
+  
+  def my_inject(n)
+    #I commented out my previous attempt so I would have it for historical sake.
+    #I looked at the test conditions and they all put in a value for the parameter.
+    #I think the real version works with our without a parameter, but for this
+    #assignment I am not going to try and check if it's nil. I need to do some
+    #research on the proper way to determine if a parameter is given or not, but
+    #I don't want to do that until I complete this project because I feel like
+    #it's cheating in a sense. I want to do this on my own if I can.
+    return to_enum(:my_inject) unless block_given? #I just noticed my previous attempt misspelled enum
+    memo = n
+    ret_value = n
     self.my_each do |v|
-      ret_value += yield(ret_value, v)
+      ret_value += yield(memo,v)
     end
-    ret_value
+      ret_value
+    #obviously my logic isn't working. I'm not sure what the problem is, but it's obvious I'm missing
+    #something. The process is working, just not correctly. My testing says 1st test should be 88, but
+    #I get 880. 2nd test expects 2227680, gets 27941760. 3rd test expects 188, gets 52080.
+    #The problem has to be in how I am collecting the results. The yield needs the memo and
+    #the current array value. That part has to be right. My mind is blank right now on
+    #why my current method of memo += is wrong and what I need to do different.
+    #OK, I am going to try and add a value ret_value which has the same initial
+    #value as memo, but is a seperate value. I feel like having memo twice might
+    #be messing with the math.
   end
 end
 
